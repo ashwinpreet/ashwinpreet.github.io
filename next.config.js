1a4bb49ef11a52ae2cc8b8f87d085aedb4f91f14
Ashwin Preetham Lobo <ashwinpreet@gmail.com>
2022-01-02 11:58:02 +0530

    NextJs Config Changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/ashwinpreet.github.io/' : '',
+  assetPrefix: isProd ? '' : '',
 	eslint: {
 		ignoreDuringBuilds: true
 	}
